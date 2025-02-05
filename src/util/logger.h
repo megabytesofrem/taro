fed67090d36ee7128343d04491d3836cd03da7d2
megabytesofrem <dhillonwilk@gmail.com>
2025-02-05 15:52:31 +0000

    fix: Fixed the logger incorrectly handling va_args

---
+++ src/util/logger.h
@@ -0,0 +1,55 @@
+#ifndef TARO_ERROR
+#define TARO_ERROR
+
+#include <stdarg.h>
+#include <stdio.h>
+
+enum LogLevel
+{
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR,
+};
+
+static inline void taro_log(enum LogLevel level, const char *fmt, va_list args)
+{
+    switch (level) {
+    case LOG_INFO:
+        printf("[INFO] ");
+        break;
+    case LOG_WARNING:
+        printf("[WARN] ");
+        break;
+    case LOG_ERROR:
+        printf("[ERROR] ");
+        break;
+    }
+
+    vprintf(fmt, args);
+}
+
+static inline void log_info(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    taro_log(LOG_INFO, fmt, args);
+    va_end(args);
+}
+
+static inline void log_warn(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    taro_log(LOG_WARNING, fmt, args);
+    va_end(args);
+}
+
+static inline void log_error(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    taro_log(LOG_ERROR, fmt, args);
+    va_end(args);
+}
+
+#endif
